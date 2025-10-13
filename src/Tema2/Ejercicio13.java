a1dd15c62b62715472b5e3cc33ce369dd06a0292
alenavzaf <navarroalejandro051@gmail.com>
2025-10-13 10:49:42 +0200

    .

--- src/Tema2/Ejercicio13.java
+++ src/Tema2/Ejercicio13.java
@@ -5,26 +5,38 @@ import java.util.Scanner;
 public class Ejercicio13 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Introduce las horas: ");
-        int horas = sc.nextInt();
-        System.out.println("Introduce los minutos: ");
-        int minutos = sc.nextInt();
-        System.out.println("Introduce los segundos: ");
-        int segundos = sc.nextInt();
-        int horasAct = horas + 1;
-        int minutosAct = minutos + 1;
-        int segundosAct = segundos + 1;
 
-        if (horas == 23) {
-            horasAct = 0;
-        } else if (minutosAct == 60) {
-           minutosAct = 0;
-        } else if (segundosAct == 60) {
-            segundosAct = 0;
+        System.out.print("Introduce las horas: ");
+        int horas = sc.nextInt();
+
+        System.out.print("Introduce los minutos: ");
+        int minutos = sc.nextInt();
+
+        System.out.print("Introduce los segundos: ");
+        int segundos = sc.nextInt();
+
+        if (horas < 0 || horas >= 24 || minutos < 0 || minutos >= 60 || segundos < 0 || segundos >= 60) {
+            System.out.println("S I N T A X   E R R O R");
+        } else {
+            segundos++;
+
+            if (segundos == 60) {
+                segundos = 0;
+                minutos++;
+            }
+
+            if (minutos == 60) {
+                minutos = 0;
+                horas++;
+            }
+
+            if (horas == 24) {
+                horas = 0;
+            }
+
+            System.out.printf("La hora actualizada es: %02d:%02d:%02d%n", horas, minutos, segundos);
         }
 
-
-        System.out.println("La hora actualizada es: " + horasAct + ":" + minutosAct + ":" + segundosAct);
         sc.close();
     }
 }
