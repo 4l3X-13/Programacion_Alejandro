0c6b75c7157341059b9696460786fe0a897682a8
alenavzaf <navarroalejandro051@gmail.com>
2025-10-20 10:24:43 +0200

    .

--- src/Tema2/Ejercicio27.java
+++ src/Tema2/Ejercicio27.java
@@ -1,10 +1,28 @@
 package Tema2;
 
-import java.util.Scanner;
-
 public class Ejercicio27 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        System.out.println("Los 20 primeros números primos: ");
 
+        int numP = 2;
+        int contador = 0;
+
+        while (contador < 20) {
+            boolean esPrimo = true;
+
+            for (int i = 2; i < numP; i++) {
+                if (numP % i == 0) {
+                    esPrimo = false;
+                    break;
+                }
+            }
+
+            if (esPrimo) {
+                System.out.println(numP);
+                contador++;
+            }
+
+            numP++;
+        }
     }
 }
