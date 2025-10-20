65494268c84cb45f74670e90bd3173d750ee1b33
alenavzaf <navarroalejandro051@gmail.com>
2025-10-20 10:32:45 +0200

    .

---
+++ src/Tema2/Ejercicio28.java
@@ -0,0 +1,29 @@
+package Tema2;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Ejercicio28 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int contador = 0;
+
+
+        Random random = new Random();
+        int randomInt = random.nextInt(10000);
+        System.out.println(randomInt);
+
+        do {
+            System.out.println("Introduce un número para ganar la lotería: ");
+            int numJ = sc.nextInt();
+            if (numJ == randomInt) {
+                System.out.println("GANASTE LA LOTERÍA");
+                break;
+            } else {
+                System.out.println("NO GANASTE :(");
+            }
+            contador++;
+
+        } while (contador < 5);
+    }
+}
