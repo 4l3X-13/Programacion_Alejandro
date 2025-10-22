568269e1bfdc1a592c0d8d395fb9abe3e1d0e695
alenavzaf <navarroalejandro051@gmail.com>
2025-10-22 13:24:04 +0200

    .

--- src/Tema2/Ejercicio23.java
+++ src/Tema2/Ejercicio23.java
@@ -6,10 +6,19 @@ public class Ejercicio23 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num;
+        int contador1 = 0;
+        int contador2 = 0;
         do {
             System.out.println("Introduce un número positivo: ");
             num = sc.nextInt();
-
+            if (num >= 0){
+            contador1= contador1 + num;
+            contador2 ++;
+            }
         } while (num >= 0);
+
+        double media = contador1/contador2;
+        System.out.println("La media de los números son: " + media);
+
     }
 }
