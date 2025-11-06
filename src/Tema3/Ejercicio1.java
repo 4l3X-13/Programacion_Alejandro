9950c6c20b70f70a18ecbac0ec109fabe32b1eaf
alenavzaf <navarroalejandro051@gmail.com>
2025-11-06 11:36:50 +0100

    .

---
+++ src/Tema3/Ejercicio1.java
@@ -0,0 +1,22 @@
+package Tema3;
+
+import java.util.Scanner;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce un número: ");
+        int num = sc.nextInt();
+        System.out.println(numberSign(num));
+    }
+
+    public static int numberSign(int num) {
+        if (num > 0) {
+            return +1;
+        } else if (num < 0) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
+}
