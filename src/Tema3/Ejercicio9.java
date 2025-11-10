257be750d3063eb52e5b81a8fd18a65a59efd3e9
alenavzaf <navarroalejandro051@gmail.com>
2025-11-10 10:25:06 +0100

    .

--- src/Tema3/Ejercicio9.java
+++ src/Tema3/Ejercicio9.java
@@ -1,7 +1,24 @@
 package Tema3;
 
+import java.util.Scanner;
+
 public class Ejercicio9 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce un caracter: ");
+        char caracter = sc.next().charAt(0);
+        System.out.println("Introduce un numero: ");
+        int numero = sc.nextInt();
+        System.out.println("\n");
+        imprimirTriangulo(caracter, numero);
+    }
 
+    public static void imprimirTriangulo(char caracter, int numLineas) {
+        for (int i = 1; i <= numLineas; i++) {
+            int numCaracteres = 2 * i - 1;
+            int espacios = numLineas - i;
+            System.out.println(" ".repeat(espacios) + String.valueOf(caracter).repeat(numCaracteres));
+
+        }
     }
 }
