f187236829295fb008827772c5e292ab93b4f886
alenavzaf <navarroalejandro051@gmail.com>
2025-11-10 09:00:52 +0100

    .

---
+++ src/Tema3/Ejercicio6.java
@@ -0,0 +1,166 @@
+package Tema3;
+
+public class Ejercicio6 {
+    public static void main(String[] args) {
+        System.out.println("TABLAS DE MULTIPLICAR");
+        tabla1();
+        System.out.println("\n");
+        tabla2();
+        System.out.println("\n");
+        tabla3();
+        System.out.println("\n");
+        tabla4();
+        System.out.println("\n");
+        tabla5();
+        System.out.println("\n");
+        tabla6();
+        System.out.println("\n");
+        tabla7();
+        System.out.println("\n");
+        tabla8();
+        System.out.println("\n");
+        tabla9();
+        System.out.println("\n");
+        tabla10();
+    }
+
+    public static void tabla1() {
+        System.out.println("TABLA 1");
+        System.out.println(1 * 1);
+        System.out.println(1 * 2);
+        System.out.println(1 * 3);
+        System.out.println(1 * 4);
+        System.out.println(1 * 5);
+        System.out.println(1 * 6);
+        System.out.println(1 * 7);
+        System.out.println(1 * 8);
+        System.out.println(1 * 9);
+        System.out.println(1 * 10);
+    }
+
+    public static void tabla2() {
+        System.out.println("TABLA 2");
+        System.out.println(2 * 1);
+        System.out.println(2 * 2);
+        System.out.println(2 * 3);
+        System.out.println(2 * 4);
+        System.out.println(2 * 5);
+        System.out.println(2 * 6);
+        System.out.println(2 * 7);
+        System.out.println(2 * 8);
+        System.out.println(2 * 9);
+        System.out.println(2 * 10);
+    }
+
+    public static void tabla3() {
+        System.out.println("TABLA 3");
+        System.out.println(3 * 1);
+        System.out.println(3 * 2);
+        System.out.println(3 * 3);
+        System.out.println(3 * 4);
+        System.out.println(3 * 5);
+        System.out.println(3 * 6);
+        System.out.println(3 * 7);
+        System.out.println(3 * 8);
+        System.out.println(3 * 9);
+        System.out.println(3 * 10);
+    }
+
+    public static void tabla4() {
+        System.out.println("TABLA 4");
+        System.out.println(4 * 1);
+        System.out.println(4 * 2);
+        System.out.println(4 * 3);
+        System.out.println(4 * 4);
+        System.out.println(4 * 5);
+        System.out.println(4 * 6);
+        System.out.println(4 * 7);
+        System.out.println(4 * 8);
+        System.out.println(4 * 9);
+        System.out.println(4 * 10);
+    }
+
+    public static void tabla5() {
+        System.out.println("TABLA 5");
+        System.out.println(5 * 1);
+        System.out.println(5 * 2);
+        System.out.println(5 * 3);
+        System.out.println(5 * 4);
+        System.out.println(5 * 5);
+        System.out.println(5 * 6);
+        System.out.println(5 * 7);
+        System.out.println(5 * 8);
+        System.out.println(5 * 9);
+        System.out.println(5 * 10);
+    }
+
+    public static void tabla6() {
+        System.out.println("TABLA 6");
+        System.out.println(6 * 1);
+        System.out.println(6 * 2);
+        System.out.println(6 * 3);
+        System.out.println(6 * 4);
+        System.out.println(6 * 5);
+        System.out.println(6 * 6);
+        System.out.println(6 * 7);
+        System.out.println(6 * 8);
+        System.out.println(6 * 9);
+        System.out.println(6 * 10);
+    }
+
+    public static void tabla7() {
+        System.out.println("TABLA 7");
+        System.out.println(7 * 1);
+        System.out.println(7 * 2);
+        System.out.println(7 * 3);
+        System.out.println(7 * 4);
+        System.out.println(7 * 5);
+        System.out.println(7 * 6);
+        System.out.println(7 * 7);
+        System.out.println(7 * 8);
+        System.out.println(7 * 9);
+        System.out.println(7 * 10);
+    }
+
+    public static void tabla8() {
+        System.out.println("TABLA 8");
+        System.out.println(8 * 1);
+        System.out.println(8 * 2);
+        System.out.println(8 * 3);
+        System.out.println(8 * 4);
+        System.out.println(8 * 5);
+        System.out.println(8 * 6);
+        System.out.println(8 * 7);
+        System.out.println(8 * 8);
+        System.out.println(8 * 9);
+        System.out.println(8 * 10);
+    }
+
+    public static void tabla9() {
+        System.out.println("TABLA 9");
+        System.out.println(9 * 1);
+        System.out.println(9 * 2);
+        System.out.println(9 * 3);
+        System.out.println(9 * 4);
+        System.out.println(9 * 5);
+        System.out.println(9 * 6);
+        System.out.println(9 * 7);
+        System.out.println(9 * 8);
+        System.out.println(9 * 9);
+        System.out.println(9 * 10);
+    }
+
+    public static void tabla10() {
+        System.out.println("TABLA 10");
+        System.out.println(10 * 1);
+        System.out.println(10 * 2);
+        System.out.println(10 * 3);
+        System.out.println(10 * 4);
+        System.out.println(10 * 5);
+        System.out.println(10 * 6);
+        System.out.println(10 * 7);
+        System.out.println(10 * 8);
+        System.out.println(10 * 9);
+        System.out.println(10 * 10);
+    }
+}
