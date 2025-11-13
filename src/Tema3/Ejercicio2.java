07448159b97e209984b12801ac459e5f59886961
alenavzaf <navarroalejandro051@gmail.com>
2025-11-13 11:43:06 +0100

    .

--- src/Tema3/Ejercicio2.java
+++ src/Tema3/Ejercicio2.java
@@ -5,25 +5,38 @@ import java.util.Scanner;
 public class Ejercicio2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int age = 0;
+
+        System.out.println("Introduce un número (para el ejercicio 1): ");
+        int num = sc.nextInt();
+        int signo = Ejercicio1.numberSign(num);
+
+        if (signo == 1) {
+            System.out.println("El número es positivo.");
+        } else if (signo == -1) {
+            System.out.println("El número es negativo.");
+        } else {
+            System.out.println("El número es cero.");
+        }
+
+        int age;
         do {
-            System.out.println("Introduce tu edad:");
+            System.out.print("Introduce tu edad: ");
             age = sc.nextInt();
             if (age < 0) {
-                System.out.println("Edad incorrecta, introduce otra edad: ");
-                age = sc.nextInt();
+                System.out.println("La edad es incorrecta. Intentalo de nuevo.");
             }
         } while (age < 0);
-        System.out.println("¿Eres mayor de edad?");
-        System.out.println(isAdult(age));
+
+        if (isAdult(age)) {
+            System.out.println("Es mayor de edad");
+        } else {
+            System.out.println("Es menor de edad");
+        }
+
         sc.close();
     }
 
     public static boolean isAdult(int age) {
-        if (age >= 18) {
-            return true;
-        } else {
-            return false;
-        }
+        return age >= 18;
     }
 }
