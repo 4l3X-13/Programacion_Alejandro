8d0a8ed4ea87d5d510f4ad49faf63d485e7e8489
alenavzaf <navarroalejandro051@gmail.com>
2025-11-13 10:36:08 +0100

    .

--- src/Tema3/Ejercicio6.java
+++ src/Tema3/Ejercicio6.java
@@ -10,16 +10,17 @@ public class Ejercicio6 {
     public static void numMult(int num) {
         while (num < 10) {
             num++;
-            System.out.println(num + " * 1= " + num * 1);
-            System.out.println(num + " * 2= " + num * 2);
-            System.out.println(num + " * 3= " + num * 3);
-            System.out.println(num + " * 4= " + num * 4);
-            System.out.println(num + " * 5= " + num * 5);
-            System.out.println(num + " * 6= " + num * 6);
-            System.out.println(num + " * 7= " + num * 7);
-            System.out.println(num + " * 8= " + num * 8);
-            System.out.println(num + " * 9= " + num * 9);
-            System.out.println(num + " * 10= " + num * 10);
+            System.out.println("\n TABLA DEL  " + num);
+            System.out.println(num + " x 1= " + num);
+            System.out.println(num + " x 2= " + num * 2);
+            System.out.println(num + " x 3= " + num * 3);
+            System.out.println(num + " x 4= " + num * 4);
+            System.out.println(num + " x 5= " + num * 5);
+            System.out.println(num + " x 6= " + num * 6);
+            System.out.println(num + " x 7= " + num * 7);
+            System.out.println(num + " x 8= " + num * 8);
+            System.out.println(num + " x 9= " + num * 9);
+            System.out.println(num + " x 10= " + num * 10);
 
         }
     }
