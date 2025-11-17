8291c57a8afa9e01e16ead37ac53dfa9454153a0
alenavzaf <navarroalejandro051@gmail.com>
2025-11-17 09:04:17 +0100

    .

---
+++ src/Tema3/ProgramacionModular2/MyMath.java
@@ -0,0 +1,28 @@
+package Tema3.ProgramacionModular2;
+
+import java.util.Scanner;
+
+public class MyMath {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduce que quieres hacer:");
+        System.out.println("Sacar el perimetro del cuadrado.");
+        System.out.println("Sacar el area del cuadrado.");
+        System.out.println("Sacar el perimetro del rectangulo.");
+        System.out.println("Sacar el area del rectangulo.");
+        System.out.println("Sacar el perimetro del circulo.");
+        System.out.println("Sacar el area del circulo.");
+        int eleccion = scanner.nextInt();
+    }
+
+
+    public double squarePerimeter(int eleccion) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Inserta la longitud del lado: ");
+        double longitud = scanner.nextDouble();
+        return
+
+    }
+
+
+}
