2e27214e0bb47d8951f437478f547af37f499e2b
alenavzaf <navarroalejandro051@gmail.com>
2025-11-27 10:46:21 +0100

    .

--- src/Tema3/Arrays/MyArray.java
+++ src/Tema3/Arrays/MyArray.java
@@ -1,7 +1,35 @@
 package Tema3.Arrays;
 
+import java.util.Scanner;
+
 public class MyArray {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int [] numeros = new int[5];
+        numeros = new int[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
+        System.out.println("1. Un procedimiento que imprime el array");
+        imprimir(numeros);
+        System.out.println("Introduce que opción quieres hacer: ");
+        int opcion = sc.nextInt();
+        switch(opcion) {
+            case 1:
+                System.out.println("Quieres imprimir el array? ");
+                boolean pregunt = sc.nextBoolean();
+                if (pregunt == true) {
+                    imprimir(numeros);
+                    break;
+                } else if (pregunt == false) {
+                    System.out.println("Pues no imprimo el array crack :(");
+                    break;
+                }
+
+        }
+    }
+
+    public static void imprimir(int [] numeros) {
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.print(numeros[i] + " ");
+        }
     }
 }
