39a08ec3e983e95b5078d056abfdf18f9e475241
alenavzaf <navarroalejandro051@gmail.com>
2025-11-27 13:14:59 +0100

    .

--- src/Tema3/Arrays/MyArray.java
+++ src/Tema3/Arrays/MyArray.java
@@ -5,14 +5,15 @@ import java.util.Scanner;
 public class MyArray {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int [] numeros = new int[5];
-        numeros = new int[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] numeros;
+        numeros = new int[]{0,1,2,3,4,5,6,7,8,9};
 
         System.out.println("1. Un procedimiento que imprime el array");
-        imprimir(numeros);
+        System.out.println("2. Una funcion que devuelva el número máximo del array: ");
+        System.out.println("3. Una funcion que devuelva el número mínimo del array: ");
         System.out.println("Introduce que opción quieres hacer: ");
         int opcion = sc.nextInt();
-        switch(opcion) {
+        switch (opcion) {
             case 1:
                 System.out.println("Quieres imprimir el array? ");
                 boolean pregunt = sc.nextBoolean();
@@ -24,12 +25,53 @@ public class MyArray {
                     break;
                 }
 
+            case 2:
+                maxArray(numeros);
+                break;
+
+            case 3:
+                minArray(numeros);
+                break;
         }
     }
 
-    public static void imprimir(int [] numeros) {
+    public static void imprimir(int[] numeros) {
         for (int i = 0; i < numeros.length; i++) {
             System.out.print(numeros[i] + " ");
         }
     }
+
+    public static void maxArray(int[] numeros) {
+        int max;
+        int max1 = 1;
+        for (int i = 0; i < numeros.length; i++) {
+            max = numeros[i];
+            if (max > max1) {
+                max1 = numeros[i];
+            }
+        }
+        System.out.println(max1);
+    }
+
+    public static void minArray(int[] numeros) {
+        int min;
+        int min1 = numeros[0];
+        for (int i = 0; i < numeros.length; i++) {
+            min = numeros[i];
+            if (min < min1) {
+                min1 = numeros[i];
+            }
+        }
+        System.out.println(min1);
+    }
+
+    public static void media(int[] numeros) {
+        int media;
+        for (int i = 0; i < numeros.length; i++) {
+            media = numeros[i];
+
+        }
+    }
+
+
 }
