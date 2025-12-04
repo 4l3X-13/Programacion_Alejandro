a440366391c9ecce89960655bbdbb43b8e12fca8
alenavzaf <navarroalejandro051@gmail.com>
2025-12-04 12:06:06 +0100

    .

--- src/Tema3/Arrays/BuscarParejas.java
+++ src/Tema3/Arrays/BuscarParejas.java
@@ -1,81 +1,70 @@
 package Tema3.Arrays;
 
-import java.util.Arrays;
-import java.util.Random;
 import java.util.Scanner;
+import java.util.Random;
 
 public class BuscarParejas {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Random random = new Random();
+        Scanner scan = new Scanner(System.in);
 
-        String[] animales = {"león", "mandril", "suricata", "pájaro", "jirafa", "cebra", "rinoceronte", "hipopótamo", "elefante", "mono"};
+        String[] cartas = {
+                "león", "león", "mandril", "mandril", "suricata", "suricata",
+                "pájaro", "pájaro", "jirafa", "jirafa", "cebra", "cebra",
+                "rinoceronte", "rinoceronte", "hipopótamo", "hipopótamo",
+                "elefante", "elefante", "mono", "mono"
+        };
 
-        String[] cartas = new String[20];
-        for (int i = 0; i < 10; i++) {
-            cartas[i] = animales[i];
-            cartas[i + 10] = animales[i];
-        }
+        mezclar(cartas);
 
-        for (int i = 0; i < cartas.length; i++) {
-            int j = random.nextInt(cartas.length);
-            String temp = cartas[i];
-            cartas[i] = cartas[j];
-            cartas[j] = temp;
-        }
-
-        boolean[] visibles = new boolean[20];
-
+        boolean[] descubiertas = new boolean[20];
         int parejasEncontradas = 0;
 
-        System.out.println("El tablero tiene 20 posiciones (0 a 19).");
-        System.out.println("Encuentra las 10 parejas.\n");
+        System.out.println("Encuentra las 10 parejas (posiciones del 0 al 19)\n");
 
         while (parejasEncontradas < 10) {
-            mostrarTablero(cartas, visibles);
+            mostrarTablero(cartas, descubiertas);
 
-            System.out.print("Elige la primera posición (0-19): ");
-            int pos1 = scanner.nextInt();
-            if (pos1 < 0 || pos1 > 19 || visibles[pos1]) {
-                System.out.println("Posición inválida o ya descubierta. Intenta de nuevo.\n");
-                continue;
-            }
+            System.out.print("Primera posición: ");
+            int p1 = scan.nextInt();
+            System.out.print("Segunda posición: ");
+            int p2 = scan.nextInt();
 
-            System.out.print("Elige la segunda posición (0-19): ");
-            int pos2 = scanner.nextInt();
-            if (pos2 < 0 || pos2 > 19 || visibles[pos2] || pos1 == pos2) {
-                System.out.println("Posición inválida, repetida o ya descubierta. Intenta de nuevo.\n");
-                continue;
-            }
+            System.out.println("\nCarta en " + p1 + ": " + cartas[p1]);
+            System.out.println("Carta en " + p2 + ": " + cartas[p2]);
 
-            System.out.println("\n¡Mirando las cartas!");
-            System.out.println("Posición " + pos1 + ": " + cartas[pos1]);
-            System.out.println("Posición " + pos2 + ": " + cartas[pos2]);
-            System.out.println();
-
-            if (cartas[pos1].equals(cartas[pos2])) {
-                visibles[pos1] = true;
-                visibles[pos2] = true;
+            if (cartas[p1] == cartas[p2]) {
+                descubiertas[p1] = true;
+                descubiertas[p2] = true;
                 parejasEncontradas++;
-                System.out.println("¡Correcto! Pareja encontrada. Llevas " + parejasEncontradas + "/10.\n");
+                System.out.println("Correcto Llevas " + parejasEncontradas + "/10.\n");
             } else {
-                System.out.println("¡No es pareja! Inténtalo de nuevo.");
-                System.out.println("\n(Presiona Enter para continuar...)");
-                scanner.nextLine();
-                scanner.nextLine();
+                // No es pareja
+                System.out.println("No es pareja. Pulsa Enter para continuar...");
+                scan.nextLine();
+                scan.nextLine();
                 for (int i = 0; i < 30; i++) System.out.println();
             }
         }
 
-        System.out.println("¡Felicidades! Has encontrado todas las parejas. ");
-        scanner.close();
+        System.out.println("Has encontrado todas las parejas.");
+        scan.close();
     }
 
-    public static void mostrarTablero(String[] cartas, boolean[] visibles) {
-        System.out.println("Tablero actual:");
+    public static void mezclar(String[] cartas) {
+        Random r = new Random();
         for (int i = 0; i < cartas.length; i++) {
-            if (visibles[i]) {
+            int j = r.nextInt(cartas.length);
+            String temp = cartas[i];
+            cartas[i] = cartas[j];
+            cartas[j] = temp;
+        }
+    }
+
+    public static void mostrarTablero(String[] cartas, boolean[] descubiertas) {
+        System.out.println("Tablero:");
+        for (int i = 0; i < cartas.length; i++) {
+            if (descubiertas[i]) {
                 System.out.print(cartas[i] + " ");
             } else {
                 System.out.print("? ");
