99b6a8736c79378cbb7596a728e1b08fbf911af3
alenavzaf <navarroalejandro051@gmail.com>
2025-12-11 11:42:47 +0100

    .

--- src/Tema3/Strings/MyString.java
+++ src/Tema3/Strings/MyString.java
@@ -3,48 +3,98 @@ package Tema3.Strings;
 import java.util.Scanner;
 
 public class MyString {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Menú de MyString: \n " +
-                "a. Una función que reciba una cadena y devuelva esta cadena invertida y en mayúsculas. \n " +
-                "b. Una función que reciba una cadena y devuelva el número de vocales. \n " +
-                "c. Una función que reciba una cadena y devuelva una palabra de mayor longitud. \n " +
-                "d. Una función que reciba dos cadenas y devuelva el número de veces que la segunda cadena está incluida en la primera. \n " +
-                "e. Una función que reciba una cadena y devuelva el número de palabras que contiene. \n " +
-                "f. Una función que reciba un número de teléfono, por ejemplo `34555332211´ y lo convierta en formato (+34)-555-332211. \n " +
-                "g. Un procedimiento que reciba una cadena y muestre por pantalla el historiograma de frecuencias de las vocales.");
-        System.out.println("Introduce la opción que quieras: ");
-        int opcion = sc.nextInt();
+        System.out.println("Menú de MyString: \n" +
+                "a. Cadena invertida y en mayúsculas. \n" +
+                "b. Contar número de vocales. \n" +
+                "c. Palabra de mayor longitud. \n" +
+                "d. Número de veces que una cadena está incluida en otra. \n" +
+                "e. Número de palabras en una cadena. \n" +
+                "f. Dar formato a número de teléfono. \n" +
+                "g. Historiograma de frecuencias de vocales.");
+        System.out.print("Introduce la opción que quieras: ");
 
-        switch (opcion){
+        char opcion = sc.next().charAt(0);
+
+        sc.nextLine();
+
+        switch (opcion) {
             case 'a':
-                System.out.println("Una función que reciba una cadena y devuelva esta cadena invertida y en mayúsculas.");
+                System.out.println("Introduce una cadena: ");
+                String cad1 = sc.nextLine();
+                System.out.println(opA(cad1));
                 break;
 
             case 'b':
-                System.out.println("Una función que reciba una cadena y devuelva el número de vocales.");
+                System.out.println("Introduce una cadena: ");
+                String cad2 = sc.nextLine();
+                System.out.println("Vocales: " + opB(cad2));
                 break;
 
             case 'c':
-                System.out.println("Una función que reciba una cadena y devuelva una palabra de mayor longitud.");
+                System.out.println("Introduce una cadena: ");
+                String cad3 = sc.nextLine();
+                System.out.println("Palabra más larga: " + opC(cad3));
                 break;
 
             case 'd':
-                System.out.println("Una función que reciba dos cadenas y devuelva el número de veces que la segunda cadena está incluida en la primera.");
+                System.out.println("Introduce la cadena principal:");
+                String cad4 = sc.nextLine();
+                System.out.println("Introduce la cadena a buscar:");
+                String cad5 = sc.nextLine();
+                System.out.println("Apariciones: " + opD(cad4, cad5));
                 break;
 
             case 'e':
-                System.out.println("Una función que reciba una cadena y devuelva el número de palabras que contiene.");
+                System.out.println("Introduce una cadena:");
+                String cad6 = sc.nextLine();
+                System.out.println("Número de palabras: " + opE(cad6));
                 break;
 
             case 'f':
-                System.out.println("Una función que reciba un número de teléfono, por ejemplo `34555332211´ y lo convierta en formato (+34)-555-332211.");
+                System.out.println("Introduce un número de teléfono:");
+                String telefono = sc.nextLine();
+                System.out.println(opF(telefono));
                 break;
 
             case 'g':
-                System.out.println("Un procedimiento que reciba una cadena y muestre por pantalla el historiograma de frecuencias de las vocales.");
+                System.out.println("Introduce una cadena:");
+                String cad7 = sc.nextLine();
+                opG(cad7);
                 break;
-        }
 
+            default:
+                System.out.println("Opción no válida.");
+        }
+    }
+
+
+    public static String opA(String cad1) {
+        return "";
+    }
+
+    public static int opB(String cad2) {
+        return 0;
+    }
+
+    public static String opC(String cad3) {
+        return "";
+    }
+
+    public static int opD(String cad1, String cad2) {
+        return 0;
+    }
+
+    public static int opE(String cad1) {
+        return 0;
+    }
+
+    public static String opF(String telefono) {
+        return "";
+    }
+
+    public static void opG(String cad1) {
     }
 }
