a394566efd09004fdf71e6d539eae4e38714223d
alenavzaf <navarroalejandro051@gmail.com>
2026-01-08 10:57:41 +0100

    .

--- src/POO/Ejercicio1/Reloj.java
+++ src/POO/Ejercicio1/Reloj.java
@@ -2,11 +2,47 @@ package POO.Ejercicio1;
 
 public class Reloj {
     Reloj reloj = new Reloj();
-    int hora ;
+    int hora;
     int minutos;
     int segundos;
 
     public Reloj() {
         this.reloj = reloj;
+    }
+
+    public void mostrar (){
+        System.out.println(reloj);
+    }
+
+    public Reloj getReloj() {
+        return reloj;
+    }
+
+    public void setReloj(Reloj reloj) {
+        this.reloj = reloj;
+    }
+
+    public int getHora() {
+        return hora;
+    }
+
+    public void setHora(int hora) {
+        this.hora = hora;
+    }
+
+    public int getMinutos() {
+        return minutos;
+    }
+
+    public void setMinutos(int minutos) {
+        this.minutos = minutos;
+    }
+
+    public int getSegundos() {
+        return segundos;
+    }
+
+    public void setSegundos(int segundos) {
+        this.segundos = segundos;
     }
 }
--- src/POO/Ejercicio1/RelojMain.java
+++ src/POO/Ejercicio1/RelojMain.java
@@ -1,7 +1,11 @@
 package POO.Ejercicio1;
 
+import java.util.Scanner;
+
 public class RelojMain {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
 
     }
 }
