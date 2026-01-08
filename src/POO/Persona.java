59daffafd36feeea2af9f715154b7a862f698b1f
alenavzaf <navarroalejandro051@gmail.com>
2026-01-08 10:41:39 +0100

    .

---
+++ src/POO/Persona.java
@@ -0,0 +1,60 @@
+package POO;
+
+public class Persona {
+
+    String nombre;
+    int edad;
+    final int edadDefecto = 0;
+
+
+    public Persona() {
+        nombre = "Alex";
+        edad = 19;
+    }
+
+    public Persona(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Persona(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public void saludar() {
+        System.out.println("¡Hola!");
+    }
+
+    public void presentarme() {
+        System.out.println("Soy " + nombre + " y tengo " + edad + " años.");
+    }
+
+    public void saludarA(String nombre) {
+        System.out.println("¡Hola " + nombre + "!");
+    }
+
+    public static void showInfo() {
+        System.out.println("Las personas tienen nombre y edad");
+
+    }
+
+    //------------------------------------------//
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+
+}
+
---
+++ src/POO/PersonaMain.java
@@ -0,0 +1,16 @@
+package POO;
+
+public class PersonaMain {
+
+    public static void main(String[] args){
+        Persona persona = new Persona();
+
+        persona.nombre = "Ana";
+        persona.edad = 35;
+
+        persona.saludar();
+        persona.presentarme();
+        persona.saludarA("Ximo");
+
+    }
+}
