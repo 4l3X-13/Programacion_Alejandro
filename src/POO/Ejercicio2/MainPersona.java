b04f3751ef0fa68bd98757507db31db0c5ea9107
alenavzaf <navarroalejandro051@gmail.com>
2026-01-12 10:00:03 +0100

    .

--- src/POO/Ejercicio2/MainPersona.java
+++ src/POO/Ejercicio2/MainPersona.java
@@ -5,32 +5,34 @@ import java.util.Scanner;
 public class MainPersona {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.printf("Introduce tu DNI: ");
+
+        System.out.print("Introduce tu DNI: ");
         String dni1 = sc.nextLine();
 
-        System.out.println("Introduce tu nombre: ");
+        System.out.print("Introduce tu nombre: ");
         String nombre1 = sc.nextLine();
 
-        System.out.println("Introduce tus apellidos: ");
+        System.out.print("Introduce tus apellidos: ");
         String apellidos1 = sc.nextLine();
 
-        System.out.println("Introduce tu edad: ");
+        System.out.print("Introduce tu edad: ");
         int edad1 = sc.nextInt();
+        sc.nextLine();
 
-//---------------------------------------------------------//
+        //---------------------------------------------------------//
 
-        System.out.printf("Introduce tu DNI: ");
+        System.out.print("Introduce tu DNI: ");
         String dni2 = sc.nextLine();
 
-        System.out.println("Introduce tu nombre: ");
+        System.out.print("Introduce tu nombre: ");
         String nombre2 = sc.nextLine();
 
-        System.out.println("Introduce tus apellidos: ");
+        System.out.print("Introduce tus apellidos: ");
         String apellidos2 = sc.nextLine();
 
-        System.out.println("Introduce tu edad: ");
+        System.out.print("Introduce tu edad: ");
         int edad2 = sc.nextInt();
 
-
+        sc.close();
     }
 }
