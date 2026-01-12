1bc975407dc30e198f83f6cfa22272dc6e61833a
alenavzaf <navarroalejandro051@gmail.com>
2026-01-12 09:10:28 +0100

    .

--- src/POO/Ejercicio2/MainPersona.java
+++ src/POO/Ejercicio2/MainPersona.java
@@ -6,12 +6,29 @@ public class MainPersona {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.printf("Introduce tu DNI: ");
-        String dni = sc.nextLine();
+        String dni1 = sc.nextLine();
+
         System.out.println("Introduce tu nombre: ");
-        String nombre = sc.nextLine();
+        String nombre1 = sc.nextLine();
+
         System.out.println("Introduce tus apellidos: ");
-        String apellidos = sc.nextLine();
+        String apellidos1 = sc.nextLine();
+
         System.out.println("Introduce tu edad: ");
-        int edad = sc.nextInt();
+        int edad1 = sc.nextInt();
+
+        System.out.printf("Introduce tu DNI: ");
+        String dni2 = sc.nextLine();
+
+        System.out.println("Introduce tu nombre: ");
+        String nombre2 = sc.nextLine();
+
+        System.out.println("Introduce tus apellidos: ");
+        String apellidos2 = sc.nextLine();
+
+        System.out.println("Introduce tu edad: ");
+        int edad2 = sc.nextInt();
+
+
     }
 }
