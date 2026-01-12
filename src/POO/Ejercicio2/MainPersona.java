29534984d023779703c2962a03f5921b53bed5f3
alenavzaf <navarroalejandro051@gmail.com>
2026-01-12 09:09:21 +0100

    .

--- src/POO/Ejercicio2/MainPersona.java
+++ src/POO/Ejercicio2/MainPersona.java
@@ -1,7 +1,17 @@
 package POO.Ejercicio2;
 
+import java.util.Scanner;
+
 public class MainPersona {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("Introduce tu DNI: ");
+        String dni = sc.nextLine();
+        System.out.println("Introduce tu nombre: ");
+        String nombre = sc.nextLine();
+        System.out.println("Introduce tus apellidos: ");
+        String apellidos = sc.nextLine();
+        System.out.println("Introduce tu edad: ");
+        int edad = sc.nextInt();
     }
 }
