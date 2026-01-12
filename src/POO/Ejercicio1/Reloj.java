efc0ccffa68e04c493052362f82bfa59a39d4a41
alenavzaf <navarroalejandro051@gmail.com>
2026-01-12 09:36:20 +0100

    .

--- src/POO/Ejercicio1/Reloj.java
+++ src/POO/Ejercicio1/Reloj.java
@@ -1,70 +1,48 @@
 package POO.Ejercicio1;
 
 public class Reloj {
-    int hora;
-    int minutos;
-    int segundos;
-    boolean es24h;
-
-    public Reloj() {
-    }
+    private int hora;
+    private int minutos;
+    private int segundos;
+    private boolean es24h;
 
     public void mostrar() {
         System.out.println("Son las " + hora + "h " + minutos + "minutos y " + segundos + "segundos");
     }
 
-
-    public int getHora() {
-        return hora;
-    }
-
     public void setHora(int hora) {
-        if (es24h == true) {
-            if (hora > 0 && hora < 24) {
+        if (es24h) {
+            if (hora >= 0 && hora <= 23) {
                 this.hora = hora;
             } else {
-                System.out.println("Hora errónea");
+                System.out.println("Hora errónea en formato 24h");
             }
         } else {
-            if (hora > 0 && hora < 12) {
+            if (hora >= 1 && hora <= 12) {
                 this.hora = hora;
             } else {
-                hora = hora - 12;
-                this.hora = hora;
+                System.out.println("Hora errónea en formato 12h");
             }
         }
     }
 
-    public int getMinutos() {
-        return minutos;
-    }
-
     public void setMinutos(int minutos) {
-        this.minutos = minutos;
-    }
-
-    public int getSegundos() {
-        return segundos;
+        if (minutos >= 0 && minutos <= 59) {
+            this.minutos = minutos;
+        } else {
+            System.out.println("Minutos erróneos");
+        }
     }
 
     public void setSegundos(int segundos) {
-        this.segundos = segundos;
-    }
-
-    public boolean isEs24h() {
-        return es24h;
+        if (segundos >= 0 && segundos <= 59) {
+            this.segundos = segundos;
+        } else {
+            System.out.println("Segundos erróneos");
+        }
     }
 
     public void setEs24h(boolean es24h) {
         this.es24h = es24h;
-    }
-
-    public String toString() {
-        return "Reloj{" +
-                "hora=" + hora +
-                ", minutos=" + minutos +
-                ", segundos=" + segundos +
-                ", es24h=" + es24h +
-                '}';
     }
 }
--- src/POO/Ejercicio1/RelojMain.java
+++ src/POO/Ejercicio1/RelojMain.java
@@ -1,16 +1,16 @@
 package POO.Ejercicio1;
 
-import java.util.Scanner;
+import POO.Ejercicio1.Reloj;
 
 public class RelojMain {
     public static void main(String[] args) {
         Reloj reloj = new Reloj();
-        Scanner sc = new Scanner(System.in);
-        reloj.setHora(13);
+
+        reloj.setEs24h(true);
+        reloj.setHora(23);
         reloj.setMinutos(45);
         reloj.setSegundos(23);
-        reloj.setEs24h(true);
-        reloj.mostrar();
 
+        reloj.mostrar();
     }
 }
