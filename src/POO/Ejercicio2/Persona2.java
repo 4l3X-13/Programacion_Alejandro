adff0f5c9ffc110276b8eba89247eee16f16db42
alenavzaf <navarroalejandro051@gmail.com>
2026-01-12 10:05:00 +0100

    .

--- src/POO/Ejercicio2/Persona2.java
+++ src/POO/Ejercicio2/Persona2.java
@@ -1,4 +1,40 @@
 package POO.Ejercicio2;
 
 public class Persona2 {
+    String dni2;
+    String nombre2;
+    String apellidos2;
+    int edad2;
+
+    public String getDni2() {
+        return dni2;
+    }
+
+    public void setDni2(String dni2) {
+        this.dni2 = dni2;
+    }
+
+    public String getNombre2() {
+        return nombre2;
+    }
+
+    public void setNombre2(String nombre2) {
+        this.nombre2 = nombre2;
+    }
+
+    public String getApellidos2() {
+        return apellidos2;
+    }
+
+    public void setApellidos2(String apellidos2) {
+        this.apellidos2 = apellidos2;
+    }
+
+    public int getEdad2() {
+        return edad2;
+    }
+
+    public void setEdad2(int edad2) {
+        this.edad2 = edad2;
+    }
 }
