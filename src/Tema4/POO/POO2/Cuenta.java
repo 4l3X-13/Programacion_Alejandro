421c4b47db9f17ede43f1a5d6d1309362ed78dde
alenavzaf <navarroalejandro051@gmail.com>
2026-01-26 09:15:38 +0100

    .

--- src/Tema4/POO/POO2/Cuenta.java
+++ src/Tema4/POO/POO2/Cuenta.java
@@ -3,4 +3,5 @@ package Tema4.POO.POO2;
 public class Cuenta {
     private int numCuenta;
     private double saldo;
+
 }
--- src/Tema4/POO/POO2/Persona.java
+++ src/Tema4/POO/POO2/Persona.java
@@ -1,4 +1,6 @@
 package Tema4.POO.POO2;
 
 public class Persona {
+    private String DNI;
+    String[] cuentaBancaria = new String[3];
 }
