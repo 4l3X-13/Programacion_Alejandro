319e35340661bbc601b8b9daceb22acbc6587c1a
alenavzaf <navarroalejandro051@gmail.com>
2026-01-26 09:49:36 +0100

    .

--- src/Tema4/POO/POO2/Cuenta.java
+++ src/Tema4/POO/POO2/Cuenta.java
@@ -1,9 +1,27 @@
 package Tema4.POO.POO2;
 
 public class Cuenta {
-    public long numCuenta= 1818181818181818181L;
-    public double saldo;
+    public long numCuenta= 1919191919191919191L;
+    public double saldo = 1000000000;
 
+    public Cuenta(long numCuenta, double saldo) {
+        this.numCuenta = numCuenta;
+        this.saldo = saldo;
+    }
 
+    public long getNumCuenta() {
+        return numCuenta;
+    }
 
+    public void setNumCuenta(long numCuenta) {
+        this.numCuenta = numCuenta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
 }
--- src/Tema4/POO/POO2/Persona.java
+++ src/Tema4/POO/POO2/Persona.java
@@ -1,6 +1,9 @@
 package Tema4.POO.POO2;
 
 public class Persona {
-    private String DNI;
+    public String DNI;
+    public String nombre;
+    public String apellido1;
+    public String apellido2;
     String[] cuentaBancaria = new String[3];
 }
