501ed83e184c0644d1d39c5b0b5a101fad63804c
alenavzaf <navarroalejandro051@gmail.com>
2026-02-02 10:08:16 +0100

    .

--- src/Tema4/POO/POO1/Ejercicio2/Persona.java
+++ src/Tema4/POO/POO1/Ejercicio2/Persona.java
@@ -1,15 +1,17 @@
 package Tema4.POO.POO1.Ejercicio2;
 
+import java.util.Scanner;
+
 public class Persona {
+    public static final int EDAD_ADULTA = 18;
+    public static final int EDAD_RETIRO = 65;
     private String dni;
     private String nombre;
     private String apellidos;
     private int edad;
 
-    public static final int EDAD_ADULTA = 18;
-    public static final int EDAD_RETIRO = 65;
-
-    public Persona() {}
+    public Persona() {
+    }
 
     public Persona(String dni, String nombre, String apellidos, int edad) {
         this.dni = dni;
@@ -18,17 +20,54 @@ public class Persona {
         this.edad = edad;
     }
 
-    public String getDni() { return dni; }
-    public void setDni(String dni) { this.dni = dni; }
+    public static String validarDNI(String dni) {
+        char[] letras = {'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'};
+        Scanner in = new Scanner(System.in);
+        System.out.println("dime los numeros del DNI:");
+        int numeroDNI = in.nextInt();
+        System.out.println("dime la letra del DNI:");
+        char letraDNI = in.next().toUpperCase().charAt(0);
+        int resto = numeroDNI % 23;
 
-    public String getNombre() { return nombre; }
-    public void setNombre(String nombre) { this.nombre = nombre; }
+        if (letras[resto] == letraDNI) {
+            return "El DNI es correcto";
+        } else {
+            return "El DNI es incorrecto. La letra debería ser: " + letras[resto];
+        }
 
-    public String getApellidos() { return apellidos; }
-    public void setApellidos(String apellidos) { this.apellidos = apellidos; }
+    }
 
-    public int getEdad() { return edad; }
-    public void setEdad(int edad) { this.edad = edad; }
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
 
     public void mostrar() {
         System.out.println(nombre + " " + apellidos + " | DNI: " + dni + " | Edad: " + edad);
@@ -44,9 +83,5 @@ public class Persona {
 
     public int diferenciaEdad(Persona otra) {
         return Math.abs(this.edad - otra.getEdad());
-    }
-
-    public static boolean validarDNI(String dni) {
-        return dni != null && dni.matches("\\d{8}[A-Za-z]");
     }
 }
