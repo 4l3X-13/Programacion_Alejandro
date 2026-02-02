32356a11828b0a32ce93b3fe3331db00556cf61c
alenavzaf <navarroalejandro051@gmail.com>
2026-02-02 09:40:46 +0100

    .

--- src/Tema5/EjerciciosColecciones1/Ejercicio1.java
+++ src/Tema5/EjerciciosColecciones1/Ejercicio1.java
@@ -1,7 +1,22 @@
 package Tema5.EjerciciosColecciones1;
 
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 public class Ejercicio1 {
     static void main() {
+        Scanner scan = new Scanner(System.in);
+        menu();
+        Set<String> productos = new HashSet<>();
 
+    }
+
+
+    public static void menu() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("-------");
+        System.out.println("CREAR LISTA DE LA COMPRA");
+        System.out.println("-------");
     }
 }
