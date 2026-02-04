b91ae3b449b9ad3cc24ccf24ec2c4c762dfeed01
alenavzaf <navarroalejandro051@gmail.com>
2026-02-04 13:35:16 +0100

    .

--- src/Tema5/EjerciciosColecciones1/Ejercicio3/Ejercicio3.java
+++ src/Tema5/EjerciciosColecciones1/Ejercicio3/Ejercicio3.java
@@ -1,7 +1,78 @@
 package Tema5.EjerciciosColecciones1.Ejercicio3;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Ejercicio3 {
     static void main() {
+        Scanner scan = new Scanner(System.in);
+        Map<String, Usuario> usuarios = new HashMap<>();
+        int opcion;
+        String dni;
 
+        do {
+            System.out.println("--------------------------------");
+            System.out.println("            BANCO ");
+            System.out.println("--------------------------------");
+            menu();
+            opcion = scan.nextInt();
+            scan.nextLine();
+
+            switch (opcion) {
+                case 1:
+                    System.out.println("Introduce tu DNI:");
+                    dni = scan.nextLine();
+
+                    System.out.println("Ahora tu nombre: ");
+                    String nombre = scan.nextLine();
+
+                    System.out.println("Y ahora por último introduce tu edad: ");
+                    int edad = scan.nextInt();
+
+                    usuarios.put(dni, new Usuario(nombre, edad, dni));
+                    System.out.println("Usuario guardado: " + usuarios.get(dni));
+                    break;
+
+                case 2:
+                    System.out.println("Introduce el DNI de la persona que quieres quitar: ");
+                    String dni2 = scan.nextLine();
+                    if (usuarios.containsKey(dni2)) {
+                        usuarios.remove(dni2);
+                        System.out.println("Usuario eliminado.");
+                    } else {
+                        System.out.println("El usuario no existe.");
+                    }
+                    break;
+
+                case 3:
+                    System.out.println("Introduce el DNI para buscar el usuario: ");
+                    dni = scan.nextLine();
+
+                    if (usuarios.containsKey(dni)) {
+                        Usuario u = usuarios.get(dni);
+                        System.out.println("Datos del usuario " + dni + ": " + u);
+                    } else {
+                        System.out.println("Usuario no existe.");
+                    }
+                    break;
+
+                case 0:
+                    System.out.println("Saliendo...");
+                    break;
+
+                default:
+                    System.out.println("Opción incorrecta");
+            }
+
+        } while (opcion != 0);
+    }
+
+    public static void menu() {
+        System.out.println("0. Salir");
+        System.out.println("1. Añadir usuario");
+        System.out.println("2. Quitar usuario");
+        System.out.println("3. Buscar usuario");
+        System.out.println("Introduce la opción que quieres:");
     }
 }
---
+++ src/Tema5/EjerciciosColecciones1/Ejercicio3/Usuario.java
@@ -0,0 +1,43 @@
+package Tema5.EjerciciosColecciones1.Ejercicio3;
+
+public class Usuario {
+    private String nombre;
+    private int edad;
+    private String dni;
+
+    public Usuario(String nombre, int edad, String dni) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario: Nombre='" + nombre + '\'' + ", edad=" + edad + ", dni='" + dni;
+    }
+}
+
