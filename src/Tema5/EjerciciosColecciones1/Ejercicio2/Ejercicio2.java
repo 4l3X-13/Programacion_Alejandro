f0546ff8bc2d28df09b97a4a2336cc039a3194e6
alenavzaf <navarroalejandro051@gmail.com>
2026-02-04 12:12:00 +0100

    .

--- src/Tema5/EjerciciosColecciones1/Ejercicio2/Ejercicio2.java
+++ src/Tema5/EjerciciosColecciones1/Ejercicio2/Ejercicio2.java
@@ -1,7 +1,13 @@
 package Tema5.EjerciciosColecciones1.Ejercicio2;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Ejercicio2 {
     static void main() {
+        Scanner scan = new Scanner(System.in);
+        Map<String, Usuario> usuarios = new HashMap<String, Usuario>();
 
     }
 }
