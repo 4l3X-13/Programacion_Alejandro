38fe7dde6afb17640dba09c1d9217eaf420eb289
alenavzaf <navarroalejandro051@gmail.com>
2026-02-18 13:34:52 +0100

    .

---
+++ src/Tema4/POO3/Ejercicio2/Animal.java
@@ -0,0 +1,15 @@
+package Tema4.POO3.Ejercicio2;
+
+public class Animal {
+    private String nombre;
+    private int edad;
+    private String raza;
+    private String color;
+
+    public Animal(String nombre, int edad, String raza, String color) {
+        this.nombre= nombre;
+        this.edad = edad;
+        this.raza = raza;
+        this.color = color;
+    }
+}
