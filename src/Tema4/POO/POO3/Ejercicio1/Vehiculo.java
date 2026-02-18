c0dce304096022c5d15d95635461e8589e3e598d
4l3X_Navarro <160244167+4l3X-13@users.noreply.github.com>
2026-02-18 13:10:33 +0100

    Update Vehiculo.java

--- src/Tema4/POO/POO3/Ejercicio1/Vehiculo.java
+++ src/Tema4/POO/POO3/Ejercicio1/Vehiculo.java
@@ -1,6 +1,57 @@
-package Tema4.POO.POO3.Ejercicio1;
+package Tema4.POO3.Ejercicio1;
 
 public class Vehiculo {
-    private int plazas;
-    private String tipoUso;
+    private String matricula;
+    private String modelo;
+    private String marca;
+    private String color;
+
+    public Vehiculo(String matricula, String modelo, String marca, String color) {
+        this.matricula = matricula;
+        this.modelo = modelo;
+        this.marca = marca;
+        this.color = color;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Vehiculo{" +
+                "matricula='" + matricula + '\'' +
+                ", modelo='" + modelo + '\'' +
+                ", marca='" + marca + '\'' +
+                ", color='" + color + '\'' +
+                '}';
+    }
 }
