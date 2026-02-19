234a9310ac6859b7c5aeaf8aedc7702711b0b420
alenavzaf <navarroalejandro051@gmail.com>
2026-02-19 11:55:06 +0100

    .

---
+++ src/Tema4/POO3/Ejercicio3/Figura2D.java
@@ -0,0 +1,14 @@
+package Tema4.POO3.Ejercicio3;
+
+public class Figura2D {
+
+    public void area() {
+    }
+    public void perimetro() {
+    }
+
+    @Override
+    public String toString() {
+        return "Figura2D{}";
+    }
+}
---
+++ src/Tema4/POO3/Ejercicio3/Rectangulo.java
@@ -0,0 +1,4 @@
+package Tema4.POO3.Ejercicio3;
+
+public class Rectangulo {
+}
