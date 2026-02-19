c940b24f20abdef1ef82f7c3a0e552058a27b3f8
alenavzaf <navarroalejandro051@gmail.com>
2026-02-19 10:45:41 +0100

    .

---
+++ src/Tema4/POO3/Ejercicio2/Foca.java
@@ -0,0 +1,21 @@
+package Tema4.POO3.Ejercicio2;
+
+public class Foca extends Animal {
+    public boolean aplaude;
+
+
+    public Foca(String nombre, int edad, String raza, String color, boolean aplaude) {
+        super(nombre, edad, raza, color);
+        this.aplaude = aplaude ;
+    }
+
+    public Foca() {
+        this("Akila", 3, "Perro", "blanco", true);
+    }
+
+    public void Ough() {
+        System.out.println("Ough!!!!");
+    }
+
+
+}
