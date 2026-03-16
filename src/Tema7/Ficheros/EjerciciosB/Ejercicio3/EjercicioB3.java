1e1011fae9ce558fa8586954bf0525bbfd36345d
alenavzaf <navarroalejandro051@gmail.com>
2026-03-16 09:11:11 +0100

    .

--- src/Tema7/Ficheros/EjerciciosB/Ejercicio3/EjercicioB3.java
+++ src/Tema7/Ficheros/EjerciciosB/Ejercicio3/EjercicioB3.java
@@ -1,7 +1,29 @@
 package Tema7.Ficheros.EjerciciosB.Ejercicio3;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
 public class EjercicioB3 {
     static void main() {
+        String fileName = "usa_personas_sorted.txt";
+        String fileName1 = "/home/alenavzaf/IdeaProjects/Programacion_Alejandro_GITHUB_COMPLETO//src/Tema7/Ficheros/EjerciciosB/Ejercicio3/usa_personas_sorted.txt";
+        Path filePath = Paths.get("usa_personas_sorted.txt");
 
+        try {
+            //Escribe contenido al fichero, borrando todo lo que haya
+            Files.writeString(filePath, "Aquí escribo borrando todo el contendio anterior");
+            //Escribe contenido al fichero, añadiendolo al final del mismo
+            Files.writeString(filePath, "Aquí escribo añadiendo al final", StandardOpenOption.APPEND);
+
+            //Verify file content
+            String content = Files.readString(filePath);
+
+            System.out.println(content);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
