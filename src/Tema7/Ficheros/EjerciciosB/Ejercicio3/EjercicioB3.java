57080bf86aea5f6501414a4544e94cc8fd9a3b7c
alenavzaf <navarroalejandro051@gmail.com>
2026-03-16 09:44:01 +0100

    .

--- src/Tema7/Ficheros/EjerciciosB/Ejercicio3/EjercicioB3.java
+++ src/Tema7/Ficheros/EjerciciosB/Ejercicio3/EjercicioB3.java
@@ -1,15 +1,25 @@
 package Tema7.Ficheros.EjerciciosB.Ejercicio3;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class EjercicioB3 {
     static void main() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Introduce el nombre del archivo: ");
+        System.out.println("Introduce el nombre del archivo origen: ");
         String archivoOrig = sc.nextLine();
-        String fileName1 = "archivo1";
+
         System.out.println("Introduce el nombre del archivo destino: ");
         String archivoDest = sc.nextLine();
-        String fileName2 = "archivo2";
+
+        List<String[]> lineas = new ArrayList<>();
+
+        try{
+
+        }catch (Exception e){
+            System.out.println("Error!!");
+        }
+
     }
 }
