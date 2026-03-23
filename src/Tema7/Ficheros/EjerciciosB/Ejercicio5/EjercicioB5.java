058769ae737b9d8d6997a6b4467ba6d92b353465
alenavzaf <navarroalejandro051@gmail.com>
2026-03-23 10:10:17 +0100

    .

--- src/Tema7/Ficheros/EjerciciosB/Ejercicio5/EjercicioB5.java
+++ src/Tema7/Ficheros/EjerciciosB/Ejercicio5/EjercicioB5.java
@@ -1,7 +1,26 @@
 package Tema7.Ficheros.EjerciciosB.Ejercicio5;
 
-public class EjercicioB5 {
-    static void main(String[] args) {
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+public class EjercicioB5 {
+    public static void main(String[] args) throws IOException {
+        Files.createDirectories(Paths.get("Diccionario"));
+        try (BufferedReader br = new BufferedReader(new FileReader("diccionario.txt"))) {
+            String palabra;
+            while ((palabra = br.readLine()) != null) {
+                if (!palabra.isEmpty()) {
+                    char letra = Character.toUpperCase(palabra.charAt(0));
+                    Files.writeString(Paths.get("Diccionario", letra + ".txt"), palabra + System.lineSeparator(),
+                            StandardOpenOption.CREATE, StandardOpenOption.APPEND);
+                }
+            }
+        }
     }
 }
+
