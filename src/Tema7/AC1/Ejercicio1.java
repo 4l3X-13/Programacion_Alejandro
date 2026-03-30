9ef81ad9548d3ecfdd81ebdefe9a8159fa0c30c7
alenavzaf <navarroalejandro051@gmail.com>
2026-03-30 10:45:34 +0200

    .

--- src/Tema7/AC1/Ejercicio1.java
+++ src/Tema7/AC1/Ejercicio1.java
@@ -1,8 +1,6 @@
 package Tema7.AC1;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.Scanner;
 
 public class Ejercicio1 {
@@ -12,11 +10,12 @@ public class Ejercicio1 {
         String nombre = sc.nextLine();
         System.out.println("Introduce tu edad: ");
         int edad = sc.nextInt();
-        try (BufferedReader br = new BufferedReader(new FileReader("/home/alenavzaf/IdeaProjects/Programacion_Alejandro_GITHUB_COMPLETO/src/Tema7/AC1/Ficheros/usuario.txt"))) {
-
-
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter("/home/alenavzaf/IdeaProjects/Programacion_Alejandro_GITHUB_COMPLETO/src/Tema7/AC1/Ficheros/usuario.txt"))) {
+            bw.write("Nombre: " + nombre);
+            bw.newLine();
+            bw.write("Edad: " + edad);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
