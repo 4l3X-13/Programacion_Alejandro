54fd0cb5eb2454011ddee69e2bfba559e07a0f70
alecssss <navarroalejandro051@gmail.com>
2026-04-07 17:01:51 +0200

    .

---
+++ src/Tema7/Funkos2/Funko.java
@@ -0,0 +1,68 @@
+package Tema7.Funkos2;
+
+public class Funko {
+    private String code;
+    private String nombre;
+    private String modelo;
+    private double precio;
+    private String fecha_lanzamiento;
+
+    public Funko(String code, String nombre, String modelo, double precio, String fecha_lanzamiento) {
+        this.code = code;
+        this.nombre = nombre;
+        this.modelo = modelo;
+        this.precio = precio;
+        this.fecha_lanzamiento = fecha_lanzamiento;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public String getFecha_lanzamiento() {
+        return fecha_lanzamiento;
+    }
+
+    public void setFecha_lanzamiento(String fecha_lanzamiento) {
+        this.fecha_lanzamiento = fecha_lanzamiento;
+    }
+
+    @Override
+    public String toString() {
+        return "Funko{" +
+                "code='" + code + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", modelo='" + modelo + '\'' +
+                ", precio=" + precio +
+                ", fecha_lanzamiento='" + fecha_lanzamiento + '\'' +
+                '}';
+    }
+}
---
+++ src/Tema7/Funkos2/FunkoApp.java
@@ -0,0 +1,17 @@
+package Tema7.Funkos2;
+
+import java.util.Scanner;
+
+public class FunkoApp {
+    static void main() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("MENÚ FUNKOS");
+        System.out.println("1. AÑADIR FUNKO");
+        System.out.println("2. BORRAR FUNKO");
+        System.out.println("3. MOSTRAR TODOS LOS FUNKOS");
+        System.out.println("4. MOSTRAR FUNKO MAS CARO");
+        System.out.println("5. MOSTRAR MEDIA DE PRECIOS DE LOS FUNKOS");
+        System.out.println("6. MOSTRAR LOS FUNKOS POR MODELOS");
+        System.out.println("7. MOSTRAR LOS FUNKOS DE 2023");
+    }
+}
