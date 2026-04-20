02ac6dd344d107bd97187279b5370927d691f63e
alenavzaf <navarroalejandro051@gmail.com>
2026-04-20 09:15:28 +0200

    .

--- src/Tema8/RepasoSQL/RepasoSQL.java
+++ src/Tema8/RepasoSQL/RepasoSQL.java
@@ -1,39 +1,44 @@
 package Tema8.RepasoSQL;
 
 import java.sql.*;
+
 public class RepasoSQL {
-    public static void main(String[] args){
-        //opción1: Statement (NO RECOMENDADO)
-        try(Connection con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/anavicianofabregat");) {
-            try(Statement stmt = con.createStatement();
-                ResultSet rs = stmt.executeQuery("SELECT * FROM personas")){
-                while(rs.next()){
-                    String dni = rs.getString("dni");
-                    String nombre = rs.getString("nombre");
-                    System.out.println("personas: " + dni + ", " + nombre);
-                }
+    public static void main(String[] args) {
+
+        String sentenciaSQL1 = "SELECT nombres, apellidos FROM profesores";
+        //opcion 2: PreparedStatement
+        try (Connection con2 = DriverManager.getConnection("jdbc:postgresql://localhost:5432/anavicianofabregat");
+             PreparedStatement sentencia = con2.prepareStatement(sentenciaSQL1)) {
+
+            //no hace falta meterlo en el try, porque se cierra automáticamente al cerrarse el PreparedStatement
+            ResultSet resultados = sentencia.executeQuery();
+
+            while (resultados.next()) {
+                String dni = resultados.getString("dni");
+                String nombre = resultados.getString("nombre");
+                System.out.println("personas: " + dni + ", " + nombre);
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
 
-        //Saco la consulta fuera, para poderla usar en el PreparedStatement del try
-        String sentenciaSQL = "SELECT * FROM personas";
+
+        String sentenciaSQL2 = "SELECT nombres, apellidos FROM profesores WHERE fecha_nacimiento > 1-01-1980";
         //opcion 2: PreparedStatement
-        try(Connection con2 = DriverManager.getConnection("jdbc:postgresql://localhost:5432/anavicianofabregat");
-            PreparedStatement sentencia = con2.prepareStatement(sentenciaSQL)){
+        try (Connection con2 = DriverManager.getConnection("jdbc:postgresql://localhost:5432/anavicianofabregat");
+             PreparedStatement sentencia = con2.prepareStatement(sentenciaSQL2)) {
 
             //no hace falta meterlo en el try, porque se cierra automáticamente al cerrarse el PreparedStatement
             ResultSet resultados = sentencia.executeQuery();
 
-            while(resultados.next()){
+            while (resultados.next()) {
                 String dni = resultados.getString("dni");
                 String nombre = resultados.getString("nombre");
                 System.out.println("personas: " + dni + ", " + nombre);
             }
-        }
-        catch (SQLException e) {
+        } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
 }
+
