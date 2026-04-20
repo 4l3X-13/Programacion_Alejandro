761f449ca3d86cd234593b991023636b5bd75cd0
alenavzaf <navarroalejandro051@gmail.com>
2026-04-20 09:28:38 +0200

    .

--- src/Tema8/RepasoSQL/RepasoSQL.java
+++ src/Tema8/RepasoSQL/RepasoSQL.java
@@ -32,18 +32,37 @@ public class RepasoSQL {
             ResultSet resultados = sentencia.executeQuery();
 
             while (resultados.next()) {
-                String dni = resultados.getString("dni");
                 String nombre = resultados.getString("nombre");
-                System.out.println("personas: " + dni + ", " + nombre);
+                String apellido = resultados.getString("apellido");
+                System.out.println("personas: " + nombre + ", " + apellido);
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
 
-        String sentenciaSQL3 = "SELECT nombres, apellidos FROM profesores ORDER BY fecha_nacimiento ASCENDENT";
+        String sentenciaSQL3 = "SELECT nombres, apellidos FROM estudiantes ORDER BY fecha_nacimiento ASCENDENT";
         //opcion 2: PreparedStatement
         try (Connection con2 = DriverManager.getConnection("jdbc:postgresql://localhost:5432/anavicianofabregat");
-             PreparedStatement sentencia = con2.prepareStatement(sentenciaSQL2)) {
+             PreparedStatement sentencia = con2.prepareStatement(sentenciaSQL3)) {
+
+            //no hace falta meterlo en el try, porque se cierra automáticamente al cerrarse el PreparedStatement
+            ResultSet resultados = sentencia.executeQuery();
+
+            while (resultados.next()) {
+                String nombre = resultados.getString("nombre");
+                String apellido = resultados.getString("apellido");
+                String fecha_nacimiento = resultados.getString("fecha_nacimiento");
+                System.out.println("estudiante: " + nombre + ", " + apellido + ", " + fecha_nacimiento);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        String sentenciaSQL4 = "SELECT * estudiantes FROM estudiantes GROUP BY casa ";
+        //opcion 2: PreparedStatement
+        try (Connection con2 = DriverManager.getConnection("jdbc:postgresql://localhost:5432/anavicianofabregat");
+             PreparedStatement sentencia = con2.prepareStatement(sentenciaSQL4)) {
 
             //no hace falta meterlo en el try, porque se cierra automáticamente al cerrarse el PreparedStatement
             ResultSet resultados = sentencia.executeQuery();
