e40492496eafd5e28b71d1e3dad998a07063c597
alenavzaf <navarroalejandro051@gmail.com>
2026-04-20 09:17:33 +0200

    .

--- src/Tema8/RepasoSQL/RepasoSQL.java
+++ src/Tema8/RepasoSQL/RepasoSQL.java
@@ -39,6 +39,25 @@ public class RepasoSQL {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+
+        String sentenciaSQL3 = "SELECT nombres, apellidos FROM profesores ORDER BY fecha_nacimiento ASCENDENT";
+        //opcion 2: PreparedStatement
+        try (Connection con2 = DriverManager.getConnection("jdbc:postgresql://localhost:5432/anavicianofabregat");
+             PreparedStatement sentencia = con2.prepareStatement(sentenciaSQL2)) {
+
+            //no hace falta meterlo en el try, porque se cierra automáticamente al cerrarse el PreparedStatement
+            ResultSet resultados = sentencia.executeQuery();
+
+            while (resultados.next()) {
+                String dni = resultados.getString("dni");
+                String nombre = resultados.getString("nombre");
+                System.out.println("personas: " + dni + ", " + nombre);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+
     }
 }
 
