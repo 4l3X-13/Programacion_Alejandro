807e62ec9d038fbde7ae49f3206dde1abe8c3ece
alenavzaf <navarroalejandro051@gmail.com>
2026-04-22 12:35:38 +0200

    .

--- src/Tema7/AC1/Ejercicio4.java
+++ src/Tema7/AC1/Ejercicio4.java
@@ -1,8 +1,34 @@
 package Tema7.AC1;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
 public class Ejercicio4 {
     static void main() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("dime el nombre: ");
+        String nombre = sc.nextLine();
+        System.out.println("dime la edad: ");
+        int edad = sc.nextInt();
 
+        Persona persona = new Persona(nombre, edad);
+        Path dir = Paths.get("/home/alenavzaf/IdeaProjects/Programacion_Alejandro/src/Tema7/AC1/Ficheros/persona.dat");
 
+        try (ObjectOutputStream ois = new ObjectOutputStream(
+                new FileOutputStream(dir.toFile()))) {
+
+            ois.writeObject(persona);
+            System.out.println("Persona guardada correctamente");
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
---
+++ src/Tema7/AC1/Persona.java
@@ -0,0 +1,37 @@
+package Tema7.AC1;
+
+import java.io.Serializable;
+
+public class Persona implements Serializable {
+    String nombre;
+    int edad;
+
+    public Persona(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "nombre='" + nombre + '\'' +
+                ", edad=" + edad +
+                '}';
+    }
+}
