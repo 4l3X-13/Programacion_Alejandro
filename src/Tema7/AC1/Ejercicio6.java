51ffd12059bf27280a59ee64d18b7418f47fc72c
alenavzaf <navarroalejandro051@gmail.com>
2026-04-22 12:40:21 +0200

    .

---
+++ src/Tema7/AC1/Ejercicio6.java
@@ -0,0 +1,33 @@
+package Tema7.AC1;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+public class Ejercicio6 {
+    public static void main(String[] args) {
+        Path ruta = Paths.get("/home/alenavzaf/IdeaProjects/Programacion_Alejandro/src/Tema7/AC1/Ficheros/personas.dat");
+        ArrayList<Persona> lista = new ArrayList<>();
+        lista.add(new Persona("Ana", 25));
+        lista.add(new Persona("Luis", 30));
+
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(ruta.toFile()))) {
+            oos.writeObject(lista);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(ruta.toFile()))) {
+            ArrayList<Persona> listaLeida = (ArrayList<Persona>) ois.readObject();
+            for (Persona p : listaLeida) {
+                System.out.println("Nombre: " + p.getNombre() + ", Edad: " + p.getEdad());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
