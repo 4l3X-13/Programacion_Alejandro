eb257aef39783051057d6ef092dd87ac46f97475
alenavzaf <navarroalejandro051@gmail.com>
2026-04-22 12:40:54 +0200

    .

--- src/Tema7/AC1/Ejercicio6.java
+++ src/Tema7/AC1/Ejercicio6.java
@@ -12,8 +12,8 @@ public class Ejercicio6 {
     public static void main(String[] args) {
         Path ruta = Paths.get("/home/alenavzaf/IdeaProjects/Programacion_Alejandro/src/Tema7/AC1/Ficheros/personas.dat");
         ArrayList<Persona> lista = new ArrayList<>();
-        lista.add(new Persona("Ana", 25));
-        lista.add(new Persona("Luis", 30));
+        lista.add(new Persona("Alex", 19));
+        lista.add(new Persona("Noah", 19));
 
         try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(ruta.toFile()))) {
             oos.writeObject(lista);
