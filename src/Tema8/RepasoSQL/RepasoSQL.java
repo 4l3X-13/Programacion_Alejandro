42b69d62c8093a8ec8eb391acabb6800c2e4a27a
alenavzaf <navarroalejandro051@gmail.com>
2026-04-27 09:32:44 +0200

    .

--- src/Tema8/RepasoSQL/RepasoSQL.java
+++ src/Tema8/RepasoSQL/RepasoSQL.java
@@ -7,7 +7,9 @@ public class RepasoSQL {
 
         //SENTENCIA 1
         String sentenciaSQL1 = "SELECT nombres, apellidos FROM profesores";
-        try (Connection con2 = DriverManager.getConnection("jdbc:postgresql://localhost:5432/anavicianofabregat");
+        try (Connection con2 = DriverManager.getConnection("jdbc:postgresql://ad-postgres.ceuozunrvsdu.us-east-1.rds.amazonaws.com:5432/hogwarts",
+                "postgres",
+                "12345678");
              PreparedStatement sentencia = con2.prepareStatement(sentenciaSQL1)) {
 
             //no hace falta meterlo en el try, porque se cierra automáticamente al cerrarse el PreparedStatement
@@ -25,7 +27,9 @@ public class RepasoSQL {
 
         //SENTENCIA 2
         String sentenciaSQL2 = "SELECT nombres, apellidos FROM profesores WHERE fecha_nacimiento > 1-01-1980";
-        try (Connection con2 = DriverManager.getConnection("jdbc:postgresql://localhost:5432/anavicianofabregat");
+        try (Connection con2 = DriverManager.getConnection("jdbc:postgresql://ad-postgres.ceuozunrvsdu.us-east-1.rds.amazonaws.com:5432/hogwarts",
+                "postgres",
+                "12345678");
              PreparedStatement sentencia = con2.prepareStatement(sentenciaSQL2)) {
 
             //no hace falta meterlo en el try, porque se cierra automáticamente al cerrarse el PreparedStatement
@@ -42,7 +46,9 @@ public class RepasoSQL {
 
         //SENTENCIA 3
         String sentenciaSQL3 = "SELECT nombres, apellidos FROM estudiantes ORDER BY fecha_nacimiento ASCENDENT";
-        try (Connection con2 = DriverManager.getConnection("jdbc:postgresql://localhost:5432/anavicianofabregat");
+        try (Connection con2 = DriverManager.getConnection("jdbc:postgresql://ad-postgres.ceuozunrvsdu.us-east-1.rds.amazonaws.com:5432/hogwarts",
+                "postgres",
+                "12345678");
              PreparedStatement sentencia = con2.prepareStatement(sentenciaSQL3)) {
 
             //no hace falta meterlo en el try, porque se cierra automáticamente al cerrarse el PreparedStatement
@@ -61,7 +67,9 @@ public class RepasoSQL {
 
         //SENTENCIA 4
         String sentenciaSQL4 = "SELECT count(id_estudiantes) as cantidad, nombre.estudiantes, apellido.estudiantes, id_casa.estudiantes, nombre_casa.estudiantes* estudiantes FROM estudiantes GROUP BY casa ";
-        try (Connection con2 = DriverManager.getConnection("jdbc:postgresql://localhost:5432/anavicianofabregat");
+        try (Connection con2 = DriverManager.getConnection("jdbc:postgresql://ad-postgres.ceuozunrvsdu.us-east-1.rds.amazonaws.com:5432/hogwarts",
+                "postgres",
+                "12345678");
              PreparedStatement sentencia = con2.prepareStatement(sentenciaSQL4)) {
 
             //no hace falta meterlo en el try, porque se cierra automáticamente al cerrarse el PreparedStatement
@@ -78,7 +86,9 @@ public class RepasoSQL {
 
         //SENTENCIA  5
         String sentenciaSQL5 = "SELECT * AVG(calificacion), Max(calificacion) FROM Estudiante_Asignatura JOIN Asignatura ON id_asignatura.Estudiante_Asignatura = id.asignatura.Asignatura WHERE nombre_asignatura = 'Pociones' ";
-        try (Connection con2 = DriverManager.getConnection("jdbc:postgresql://localhost:5432/anavicianofabregat");
+        try (Connection con2 = DriverManager.getConnection("jdbc:postgresql://ad-postgres.ceuozunrvsdu.us-east-1.rds.amazonaws.com:5432/hogwarts",
+                "postgres",
+                "12345678");
              PreparedStatement sentencia = con2.prepareStatement(sentenciaSQL5)) {
 
             //no hace falta meterlo en el try, porque se cierra automáticamente al cerrarse el PreparedStatement
@@ -96,7 +106,9 @@ public class RepasoSQL {
 
         //SENTENCIA  6
         String sentenciaSQL6 = "SELECT DISTINCT anyo_curso FROM Estudiante  ";
-        try (Connection con2 = DriverManager.getConnection("jdbc:postgresql://localhost:5432/anavicianofabregat");
+        try (Connection con2 = DriverManager.getConnection("jdbc:postgresql://ad-postgres.ceuozunrvsdu.us-east-1.rds.amazonaws.com:5432/hogwarts",
+                "postgres",
+                "12345678");
              PreparedStatement sentencia = con2.prepareStatement(sentenciaSQL6)) {
 
             //no hace falta meterlo en el try, porque se cierra automáticamente al cerrarse el PreparedStatement
@@ -112,7 +124,9 @@ public class RepasoSQL {
 
         //SENTENCIA  7
         String sentenciaSQL7 = "SELECT nombre,apellido FROM Estudiante WHERE apellido LIKE 'P%' ";
-        try (Connection con2 = DriverManager.getConnection("jdbc:postgresql://localhost:5432/anavicianofabregat");
+        try (Connection con2 = DriverManager.getConnection("jdbc:postgresql://ad-postgres.ceuozunrvsdu.us-east-1.rds.amazonaws.com:5432/hogwarts",
+                "postgres",
+                "12345678");
              PreparedStatement sentencia = con2.prepareStatement(sentenciaSQL7)) {
 
             //no hace falta meterlo en el try, porque se cierra automáticamente al cerrarse el PreparedStatement
@@ -128,7 +142,9 @@ public class RepasoSQL {
 
         //SENTENCIA  8
         String sentenciaSQL8 = "SELECT nombre, apellido FROM Estudiante WHERE anyo_curso IN (4,5)";
-        try (Connection con2 = DriverManager.getConnection("jdbc:postgresql://localhost:5432/anavicianofabregat");
+        try (Connection con2 = DriverManager.getConnection("jdbc:postgresql://ad-postgres.ceuozunrvsdu.us-east-1.rds.amazonaws.com:5432/hogwarts",
+                "postgres",
+                "12345678");
              PreparedStatement sentencia = con2.prepareStatement(sentenciaSQL8)) {
 
             //no hace falta meterlo en el try, porque se cierra automáticamente al cerrarse el PreparedStatement
@@ -145,7 +161,9 @@ public class RepasoSQL {
 
         //SENTENCIA  9
         String sentenciaSQL9 = "SELECT nombre, apellido FROM Estudiantes JOIN casa ON id_casa.Estudiantes WHERE anyo_curso = 5 AND nombre_casa = 'Gryffindor' OR 'Slytherin'  ";
-        try (Connection con2 = DriverManager.getConnection("jdbc:postgresql://localhost:5432/anavicianofabregat");
+        try (Connection con2 = DriverManager.getConnection("jdbc:postgresql://ad-postgres.ceuozunrvsdu.us-east-1.rds.amazonaws.com:5432/hogwarts",
+                "postgres",
+                "12345678");
              PreparedStatement sentencia = con2.prepareStatement(sentenciaSQL9)) {
 
             //no hace falta meterlo en el try, porque se cierra automáticamente al cerrarse el PreparedStatement
@@ -162,7 +180,9 @@ public class RepasoSQL {
 
         //SENTENCIA  10
         String sentenciaSQL10 = "SELECT * FROM estudiantes ORDER BY fecha_nacimiento LIMIT 5";
-        try (Connection con2 = DriverManager.getConnection("jdbc:postgresql://localhost:5432/anavicianofabregat");
+        try (Connection con2 = DriverManager.getConnection("jdbc:postgresql://ad-postgres.ceuozunrvsdu.us-east-1.rds.amazonaws.com:5432/hogwarts",
+                "postgres",
+                "12345678");
              PreparedStatement sentencia = con2.prepareStatement(sentenciaSQL10)) {
 
             //no hace falta meterlo en el try, porque se cierra automáticamente al cerrarse el PreparedStatement
@@ -180,7 +200,9 @@ public class RepasoSQL {
 
         //SENTENCIA  11
         String sentenciaSQL11 = "SELECT nombres FROM estudiantes_Asignatura JOIN Asignatura ON id_asignatura.Estudiante_asignatura = id_asignatura.Asignatura WHERE nombre_asignatura = 'Vuelo' AND calificacion >= 8";
-        try (Connection con2 = DriverManager.getConnection("jdbc:postgresql://localhost:5432/anavicianofabregat");
+        try (Connection con2 = DriverManager.getConnection("jdbc:postgresql://ad-postgres.ceuozunrvsdu.us-east-1.rds.amazonaws.com:5432/hogwarts",
+                "postgres",
+                "12345678");
              PreparedStatement sentencia = con2.prepareStatement(sentenciaSQL11)) {
 
             //no hace falta meterlo en el try, porque se cierra automáticamente al cerrarse el PreparedStatement
@@ -196,7 +218,9 @@ public class RepasoSQL {
 
         //SENTENCIA  12
         String sentenciaSQL12 = "INSERT INTO estudiante (nombre, apellido, id_casa, anyo_curso, fecha_nacimiento) VALUES (Nymphadora, Tonks, 4, 7,1973-11-25) ";
-        try (Connection con2 = DriverManager.getConnection("jdbc:postgresql://localhost:5432/anavicianofabregat");
+        try (Connection con2 = DriverManager.getConnection("jdbc:postgresql://ad-postgres.ceuozunrvsdu.us-east-1.rds.amazonaws.com:5432/hogwarts",
+                "postgres",
+                "12345678");
              PreparedStatement sentencia = con2.prepareStatement(sentenciaSQL12)) {
 
             //no hace falta meterlo en el try, porque se cierra automáticamente al cerrarse el PreparedStatement
@@ -217,7 +241,9 @@ public class RepasoSQL {
 
         //SENTENCIA  13
         String sentenciaSQL13 = ("UPDATE casa SET id_jefe = (SELECT id_profesor FROM Profesor WHERE nombre = 'Pomona' AND apellido = 'Sprout') WHERE nombre_casa = 'Hufflepuff' ");
-        try (Connection con2 = DriverManager.getConnection("jdbc:postgresql://localhost:5432/anavicianofabregat");
+        try (Connection con2 = DriverManager.getConnection("jdbc:postgresql://ad-postgres.ceuozunrvsdu.us-east-1.rds.amazonaws.com:5432/hogwarts",
+                "postgres",
+                "12345678");
              PreparedStatement sentencia = con2.prepareStatement(sentenciaSQL13)) {
 
             //no hace falta meterlo en el try, porque se cierra automáticamente al cerrarse el PreparedStatement
@@ -234,7 +260,9 @@ public class RepasoSQL {
 
         //SENTENCIA  14
         String sentenciaSQL14 = ("DELETE FROM estudiante WHERE nombre = 'Tom' AND apellido = 'Riddle' ");
-        try (Connection con2 = DriverManager.getConnection("jdbc:postgresql://localhost:5432/anavicianofabregat");
+        try (Connection con2 = DriverManager.getConnection("jdbc:postgresql://ad-postgres.ceuozunrvsdu.us-east-1.rds.amazonaws.com:5432/hogwarts",
+                "postgres",
+                "12345678");
              PreparedStatement sentencia = con2.prepareStatement(sentenciaSQL14)) {
 
             //no hace falta meterlo en el try, porque se cierra automáticamente al cerrarse el PreparedStatement
@@ -243,7 +271,7 @@ public class RepasoSQL {
             while (resultados.next()) {
                 String nombre = resultados.getString("nombre");
                 String apellido = resultados.getString("apellido");
-                System.out.println("nombre eliminado: " + nombre +  "apellido: " + apellido);
+                System.out.println("nombre eliminado: " + nombre + "apellido: " + apellido);
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
@@ -251,7 +279,9 @@ public class RepasoSQL {
 
         //SENTENCIA  15
         String sentenciaSQL15 = ("SELECT nombre.Estudiante, apellidos.Estudiante, nombre.Casa FROM Estudiante JOIN Casa ON id_casa.Estudiante = id_casa.Casa");
-        try (Connection con2 = DriverManager.getConnection("jdbc:postgresql://localhost:5432/anavicianofabregat");
+        try (Connection con2 = DriverManager.getConnection("jdbc:postgresql://ad-postgres.ceuozunrvsdu.us-east-1.rds.amazonaws.com:5432/hogwarts",
+                "postgres",
+                "12345678");
              PreparedStatement sentencia = con2.prepareStatement(sentenciaSQL15)) {
 
             //no hace falta meterlo en el try, porque se cierra automáticamente al cerrarse el PreparedStatement
@@ -260,7 +290,7 @@ public class RepasoSQL {
             while (resultados.next()) {
                 String nombre = resultados.getString("nombre");
                 String apellido = resultados.getString("apellido");
-                System.out.println("nombre eliminado: " + nombre +  "apellido: " + apellido);
+                System.out.println("nombre eliminado: " + nombre + "apellido: " + apellido);
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
@@ -268,7 +298,9 @@ public class RepasoSQL {
 
         //SENTENCIA  16
         String sentenciaSQL16 = ("SELECT nombre.Estudiante, nombre.Mascota, nombre.Asignatura ");
-        try (Connection con2 = DriverManager.getConnection("jdbc:postgresql://localhost:5432/anavicianofabregat");
+        try (Connection con2 = DriverManager.getConnection("jdbc:postgresql://ad-postgres.ceuozunrvsdu.us-east-1.rds.amazonaws.com:5432/hogwarts",
+                "postgres",
+                "12345678");
              PreparedStatement sentencia = con2.prepareStatement(sentenciaSQL16)) {
 
             //no hace falta meterlo en el try, porque se cierra automáticamente al cerrarse el PreparedStatement
@@ -277,7 +309,7 @@ public class RepasoSQL {
             while (resultados.next()) {
                 String nombre = resultados.getString("nombre");
                 String apellido = resultados.getString("apellido");
-                System.out.println("nombre eliminado: " + nombre +  "apellido: " + apellido);
+                System.out.println("nombre eliminado: " + nombre + "apellido: " + apellido);
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
