56225c482d67574a9ee09559e484b4a940135c8e
alenavzaf <navarroalejandro051@gmail.com>
2026-04-27 09:11:33 +0200

    .

---
+++ src/Tema8/RepasoSQL/Conexion3.java
@@ -0,0 +1,23 @@
+package Tema8.RepasoSQL;
+
+import java.sql.*;
+
+public class Conexion3 {
+    static void main() {
+        String sentenciaSQL = ("SELECT * FROM Profesor");
+        try (Connection con2 = DriverManager.getConnection("jdbc:postgresql://localhost:5432/anavicianofabregat");
+             PreparedStatement sentencia = con2.prepareStatement(sentenciaSQL)) {
+
+            //no hace falta meterlo en el try, porque se cierra automáticamente al cerrarse el PreparedStatement
+            ResultSet resultados = sentencia.executeQuery();
+
+            while (resultados.next()) {
+                String nombre = resultados.getString("nombre");
+                String apellido = resultados.getString("apellido");
+                System.out.println("nombre eliminado: " + nombre +  "apellido: " + apellido);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
