aeb15dd480b90c37799e64c410e416375d193cf7
alenavzaf <navarroalejandro051@gmail.com>
2026-04-27 09:27:32 +0200

    .

--- src/Tema8/RepasoSQL/Conexion3.java
+++ src/Tema8/RepasoSQL/Conexion3.java
@@ -5,7 +5,10 @@ import java.sql.*;
 public class Conexion3 {
     static void main() {
         String sentenciaSQL = ("SELECT * FROM Profesor");
-        try (Connection con2 = DriverManager.getConnection("jdbc:postgresql://ad-postgres.ceuozunrvsdu.us-east-1.rds.amazonaws.com:5432/postgres");
+        try (Connection con2 = DriverManager.getConnection(
+                "jdbc:postgresql://ad-postgres.ceuozunrvsdu.us-east-1.rds.amazonaws.com:5432/postgres",
+                "postgres",
+                "12345678");
              PreparedStatement sentencia = con2.prepareStatement(sentenciaSQL)) {
 
             //no hace falta meterlo en el try, porque se cierra automáticamente al cerrarse el PreparedStatement
@@ -14,7 +17,7 @@ public class Conexion3 {
             while (resultados.next()) {
                 String nombre = resultados.getString("nombre");
                 String apellido = resultados.getString("apellido");
-                System.out.println("nombre eliminado: " + nombre +  "apellido: " + apellido);
+                System.out.println("nombre eliminado: " + nombre + "apellido: " + apellido);
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
