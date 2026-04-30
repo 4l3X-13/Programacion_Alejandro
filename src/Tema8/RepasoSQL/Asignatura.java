866f81334a614123737618e78cfdcb5a5112f405
alenavzaf <navarroalejandro051@gmail.com>
2026-04-30 12:12:38 +0200

    .

--- src/Tema8/RepasoSQL/Asignatura.java
+++ src/Tema8/RepasoSQL/Asignatura.java
@@ -1,7 +1,46 @@
 package Tema8.RepasoSQL;
 
 public class Asignatura {
-    static void main() {
+    public String id;
+    public String nombre;
+    public int aula;
 
+    public Asignatura(String id, String nombre, int aula) {
+        this.id = id;
+        this.nombre = nombre;
+        this.aula = aula;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getAula() {
+        return aula;
+    }
+
+    public void setAula(int aula) {
+        this.aula = aula;
+    }
+
+    @Override
+    public String toString() {
+        return "Asignatura{" +
+                "id='" + id + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", aula=" + aula +
+                '}';
     }
 }
