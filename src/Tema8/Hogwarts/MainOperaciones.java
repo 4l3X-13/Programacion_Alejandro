016dff17ce8b854343161e1d5a994de844ba796f
alenavzaf <navarroalejandro051@gmail.com>
2026-05-06 12:19:48 +0200

    .

--- src/Tema8/Hogwarts/MainOperaciones.java
+++ src/Tema8/Hogwarts/MainOperaciones.java
@@ -1,7 +1,27 @@
 package Tema8.Hogwarts;
 
+import java.sql.*;
+
 public class MainOperaciones {
     static void main() {
+        System.out.println("\n SENTENCIA 1: \n");
+        String sentenciaSQL1 = "SELECT nombre, apellido FROM Estudiante WHERE Casa == 'Gryfindor' ";
+        try (Connection con2 = DriverManager.getConnection("jdbc:postgresql://ad-postgres.ceuozunrvsdu.us-east-1.rds.amazonaws.com:5432/hogwarts",
+                "postgres",
+                "12345678");
+             PreparedStatement sentencia = con2.prepareStatement(sentenciaSQL1)) {
 
+            //no hace falta meterlo en el try, porque se cierra automáticamente al cerrarse el PreparedStatement
+            ResultSet resultados = sentencia.executeQuery();
+
+            while (resultados.next()) {
+                String nombre_estudiante = resultados.getString("nombre_estudiante");
+                String apellido_estudiante = resultados.getString("apellido_estudiante");
+                System.out.println("nombre_estudiante: " + nombre_estudiante + "apellido_estudiante: " + apellido_estudiante);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
+
