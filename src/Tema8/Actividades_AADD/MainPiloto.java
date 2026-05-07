1f84dc861f99ba5f20fbb790a1fc6373ad08e6a0
alenavzaf <navarroalejandro051@gmail.com>
2026-05-07 11:48:47 +0200

    .

--- src/Tema8/Actividades_AADD/MainPiloto.java
+++ src/Tema8/Actividades_AADD/MainPiloto.java
@@ -8,10 +8,7 @@ public class MainPiloto {
     private static final Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.println("╔═════════════════════════════════════╗");
-        System.out.println("║    GESTIÓN DE PILOTOS – F1 2006     ║");
-        System.out.println("╚═════════════════════════════════════╝");
-
+        System.out.println("    GESTIÓN DE PILOTOS – F1 2006     ");
         boolean running = true;
         while (running) {
             printMenu();
